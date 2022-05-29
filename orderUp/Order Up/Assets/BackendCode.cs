37404b1a71764dd0aee0bbfcb6c00652ad75cc0e
MrKami <mashhood.anwar@gmail.com>
2022-05-29 04:31:21 -0400

    added text and microphone icon

--- orderUp/Order Up/Assets/BackendCode.cs
+++ orderUp/Order Up/Assets/BackendCode.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BackendCode : MonoBehaviour
+
 {
+
+    public TextMeshProUGUI statusText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        statusText.text = "";
     }
 
     // Update is called once per frame
@@ -17,11 +22,11 @@ public class BackendCode : MonoBehaviour
     }
 
     public void OnPointerDown() {
-        print("holding");
+        statusText.text = "Listening for your order...";
     }
 
     public void OnPointerUp() {
-        print("no longer holding");
+        statusText.text = "";
     }
     
 }
