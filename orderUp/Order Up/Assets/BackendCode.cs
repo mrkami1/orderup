6fec084f5827805071d34fb1dd8bddec9c68946c
MrKami <mashhood.anwar@gmail.com>
2022-06-01 16:57:33 -0400

    added audio recording

--- orderUp/Order Up/Assets/BackendCode.cs
+++ orderUp/Order Up/Assets/BackendCode.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using TMPro;
 
 public class BackendCode : MonoBehaviour
-
 {
 
     public TextMeshProUGUI statusText;
     public GameObject orderCanvas;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         statusText.text = "";
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,13 +25,24 @@ public class BackendCode : MonoBehaviour
 
     public void OnPointerDown() {
         statusText.text = "Listening for your order...";
+        StartRecording();
     }
 
     public void OnPointerUp() {
         statusText.text = "Processing your order...";
-        StartCoroutine(delay(2.34f));
+        StopRecording();
+        
     }
     
+    public void StartRecording() {
+        audioSource.clip = Microphone.Start("", true, 30, 44100);
+    }
+
+    public void StopRecording() {
+        Microphone.End("");
+        audioSource.Play();
+    }
+
     IEnumerator delay(float time) {
         yield return new WaitForSeconds(time);
 
