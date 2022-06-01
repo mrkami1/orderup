9765a7595818f9d4648e0f89ebe73d96512780ef
coolmanbat2 <thanusun.thayapara@mail.utoronto.ca>
2022-06-01 17:06:20 -0400

    backend code for assemblyai

--- orderUp/Order Up/Assets/BackendCode.cs
+++ orderUp/Order Up/Assets/BackendCode.cs
@@ -6,7 +6,7 @@ using TMPro;
 public class BackendCode : MonoBehaviour
 
 {
-
+    string api_key = "e31e874c4c0b400c956dcf5341857159";
     public TextMeshProUGUI statusText;
     public GameObject orderCanvas;
 
@@ -22,13 +22,50 @@ public class BackendCode : MonoBehaviour
         
     }
 
+    private string convertToLink(string audioFile) {
+        string link = "";
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri("https://api.assemblyai.com/v2/");
+        client.DefaultRequestHeaders.Add("authorization", api_key);
+
+        string jsonResult = sendRecording(client, @"/path/to/foo.wav").Result;
+        return jsonResult;
+    }
+
+    private static async Task sendRecording(HttpClient client, string link) {
+        try {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "upload");
+            request.Headers.Add("Transer-Encoding", "chunked");
+
+            var fileReader = System.IO.File.OpenRead(filePath);
+            var streamContent = new StreamContent(fileReader);
+            request.Content = streamContent;
+
+            HttpResponseMessage response = await client.SendAsync(request);
+            return await response.Content.ReadAsStringAsync();
+        }
+        catch (Exception ex) {
+            System.Console.WriteLine($"Exception: {ex.Message}");
+            throw;
+        }
+    }
+
     public void OnPointerDown() {
         statusText.text = "Listening for your order...";
+
+        // Microphone setup here.
+
+
+
     }
 
     public void OnPointerUp() {
         statusText.text = "Processing your order...";
         StartCoroutine(delay(2.34f));
+        
+        string fileType = "";
+
+        sendRecording(fileType);
     }
     
     IEnumerator delay(float time) {
